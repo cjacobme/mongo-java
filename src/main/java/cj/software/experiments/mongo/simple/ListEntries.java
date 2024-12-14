6364dd81878d98b1f363e4692dee17997565ce3f
cjacobme <cjacobme@aol.com>
2024-12-14 23:52:55 +0100

    aus allen Einträgen die wichtigen Informationen rausziehen

--- src/main/java/cj/software/experiments/mongo/simple/ListEntries.java
+++ src/main/java/cj/software/experiments/mongo/simple/ListEntries.java
@@ -10,6 +10,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 
+import java.util.Date;
+import java.util.Map;
+
 public class ListEntries {
 
     private static final Logger LOGGER = LogManager.getFormatterLogger();
@@ -21,12 +24,15 @@ public class ListEntries {
             MongoCollection<Document> log4j2Collection = mongoDatabase.getCollection("log4j2");
             FindIterable<Document> iterable = log4j2Collection.find(new Document("contextMap.correlation-id", "3185fec7-0ffc-428d-90a0-662c93c64c5c"));
             try (MongoCursor<Document> cursor = iterable.iterator()) {
-                int count = 0;
-                while (cursor.hasNext() && count < 3) {
+                while (cursor.hasNext()) {
                     Document next = cursor.next();
-                    String json = next.toJson();
-                    LOGGER.info(json);
-                    count++;
+                    @SuppressWarnings("unchecked")
+                    Map<String, String> contextMap = next.get("contextMap", Map.class);
+                    String indent = contextMap.get("indent");
+                    String correlationId = contextMap.get("correlation-id");
+                    Date timestamp = next.getDate("date");
+                    String message = next.getString("message");
+                    LOGGER.info("%s: %s %s%s", timestamp, correlationId, indent, message);
                 }
             }
         }
